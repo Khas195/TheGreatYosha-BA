4e0d0e9ee1cc99d5bcb42875148d157b6cdf97a3
Khassian <tungthanhcao1091995@gmail.com>
2021-07-22 11:56:27 +0200

    grid

--- BA-Production-Nick-Tung/Assets/Scripts/Core/2D/Pathfinding/Grid.cs
+++ BA-Production-Nick-Tung/Assets/Scripts/Core/2D/Pathfinding/Grid.cs
@@ -7,132 +7,130 @@ using UnityEngine.Tilemaps;
 [ExecuteInEditMode]
 public class Grid : MonoBehaviour
 {
-    [SerializeField]
-    bool showGizmos = false;
-    [SerializeField]
-    LayerMask unwalkableMask;
-    [SerializeField]
-    [ReadOnly]
-    Vector2 gridWorldSize;
-    [SerializeField]
-    float nodeRadius;
+	[SerializeField]
+	bool showGizmos = false;
+	[SerializeField]
+	LayerMask unwalkableMask;
+	[SerializeField]
+	[ReadOnly]
+	Vector2 gridWorldSize;
+	[SerializeField]
+	[OnValueChanged("CreateGrid")]
+	Vector2 tileSize = new Vector2();
+	[SerializeField]
+	[OnValueChanged("CreateGrid")]
+	float gridRotateDegree = 90;
+	[SerializeField]
+	float gizmosCenterSize = 0.3f;
 
-    [SerializeField]
-    [ReadOnly]
-    float nodeDiameter;
+	Node[,] grid = null;
+	[SerializeField]
+	int gridSizeX, gridSizeY;
 
-    Node[,] grid = null;
-    [SerializeField]
-    int gridSizeX, gridSizeY;
+	private void Awake()
+	{
+		CreateGrid();
+	}
 
-    private void Awake()
-    {
-        CreateGrid();
-    }
+	private void CalculateGridSize()
+	{
+		gridWorldSize.x = gridSizeX * tileSize.x;
+		gridWorldSize.y = gridSizeY * tileSize.y;
+	}
+	public Vector2 ConvertWorldToIsometricPos(int x, int y, Vector2 origin)
+	{
+		var result = new Vector2(origin.x + (x - y) * (tileSize.x / 2), origin.y + (x + y) * (tileSize.y / 2));
+		result = RotatePoints(result, gridRotateDegree);
+		return result;
+	}
+	public Vector2 RotatePoints(Vector2 oldPoint, float angle)
+	{
+		Vector2 newPoint = new Vector2(oldPoint.x, oldPoint.y);
+		newPoint.x = oldPoint.x * Mathf.Cos(angle) - oldPoint.y * Mathf.Sin(angle);
+		newPoint.y = oldPoint.x * Mathf.Sin(angle) + oldPoint.y * Mathf.Cos(angle);
+		return newPoint;
+	}
 
-    private void CalculateGridSize()
-    {
-        nodeDiameter = nodeRadius * 2;
-        gridWorldSize.x = gridSizeX * nodeDiameter;
-        gridWorldSize.y = gridSizeY * nodeDiameter;
-    }
+	[Button]
+	private void CreateGrid()
+	{
 
-    [Button]
-    private void CreateGrid()
-    {
+		CalculateGridSize();
+		grid = new Node[gridSizeX, gridSizeY];
+		var origin = this.transform.position;
+		for (int x = 0; x < gridSizeX; x++)
+		{
+			for (int y = 0; y < gridSizeY; y++)
+			{
+				Vector2[] vertices = new Vector2[4];
+				vertices[0] = ConvertWorldToIsometricPos(x, y, origin);
+				vertices[1] = ConvertWorldToIsometricPos(x + 1, y, origin);
+				vertices[2] = ConvertWorldToIsometricPos(x + 1, y + 1, origin);
+				vertices[3] = ConvertWorldToIsometricPos(x, y + 1, origin);
+				var centroid = Vector2.zero;
+				for (int i = 0; i < vertices.Length; i++)
+				{
+					centroid += vertices[i];
+				}
+				centroid = centroid / vertices.Length;
+				grid[x, y] = new Node(true, centroid, x, y, vertices);
+			}
+		}
+	}
 
-        CalculateGridSize();
-        grid = new Node[gridSizeX, gridSizeY];
-        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.up * gridWorldSize.y / 2;
-        for (int x = 0; x < gridSizeX; x++)
-        {
-            for (int y = 0; y < gridSizeY; y++)
-            {
-                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.up * (y * nodeDiameter + nodeRadius);
-                bool walkable = !(Physics2D.OverlapBox(worldPoint, new Vector2(nodeRadius, nodeRadius), 0, unwalkableMask));
-                grid[x, y] = new Node(walkable, worldPoint, x, y);
-            }
-        }
-    }
+	public List<Node> GetNeighbourNodes(Node node)
+	{
+		List<Node> neighbours = new List<Node>();
+		for (int x = -1; x <= 1; x++)
+		{
+			for (int y = -1; y <= 1; y++)
+			{
+				if (x == 0 && y == 0) continue;
+				int checkX = node.gridX + x;
+				int checkY = node.gridY + y;
 
-    public List<Node> GetNeighbourNodes(Node node)
-    {
-        List<Node> neighbours = new List<Node>();
-        for (int x = -1; x <= 1; x++)
-        {
-            for (int y = -1; y <= 1; y++)
-            {
-                if (x == 0 && y == 0) continue;
-                int checkX = node.gridX + x;
-                int checkY = node.gridY + y;
-
-                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
-                {
-                    neighbours.Add(grid[checkX, checkY]);
-                }
-            }
-        }
-        return neighbours;
-    }
-
-    [SerializeField]
-    void UpdateGridState()
-    {
-        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.up * gridWorldSize.y / 2;
-        for (int x = 0; x < gridSizeX; x++)
-        {
-            for (int y = 0; y < gridSizeY; y++)
-            {
-                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.up * (y * nodeDiameter + nodeRadius);
-                bool walkable = !(Physics2D.OverlapCircle(worldPoint, nodeRadius, unwalkableMask));
-                grid[x, y].walkable = walkable;
-            }
-        }
-
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 0));
+				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+				{
+					neighbours.Add(grid[checkX, checkY]);
+				}
+			}
+		}
+		return neighbours;
+	}
 
 
-        if (grid != null && showGizmos == true)
-        {
-            foreach (var n in grid)
-            {
+	private void OnDrawGizmos()
+	{
+		if (grid != null && showGizmos == true)
+		{
+			foreach (var n in grid)
+			{
+				n.DrawGizmos(gizmosCenterSize);
+			}
+		}
+	}
+	public Node GetNodeFromWorldPoint(Vector3 worldPosition)
+	{
+		float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+		float percentY = (worldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y;
+		percentX = Mathf.Clamp01(percentX);
+		percentY = Mathf.Clamp01(percentY);
 
-                Gizmos.color = (n.walkable) ? Color.white : Color.red;
-                Gizmos.DrawWireCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.05f));
-            }
-        }
-    }
-    public Node GetNodeFromWorldPoint(Vector3 worldPosition)
-    {
-        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
-        float percentY = (worldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y;
-        percentX = Mathf.Clamp01(percentX);
-        percentY = Mathf.Clamp01(percentY);
+		int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+		int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
 
-        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
-        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+		return grid[x, y];
 
-        return grid[x, y];
+	}
+	public int MaxSize
+	{
+		get
+		{
+			return gridSizeX * gridSizeY;
+		}
+	}
+	private void Update()
+	{
 
-    }
-    public int MaxSize
-    {
-        get
-        {
-            return gridSizeX * gridSizeY;
-        }
-    }
-    private void Update()
-    {
-        if (grid != null)
-        {
-            UpdateGridState();
-        }
-
-    }
+	}
 }
--- BA-Production-Nick-Tung/Assets/Scripts/Core/2D/Pathfinding/Node.cs
+++ BA-Production-Nick-Tung/Assets/Scripts/Core/2D/Pathfinding/Node.cs
@@ -6,62 +6,79 @@ using UnityEngine;
 [Serializable]
 public class Node : IHeapItem<Node>
 {
-    public bool walkable;
-    public Vector3 worldPosition;
-    public int gridX = 0;
-    public int gridY = 0;
-    public int gCost;
-    public int hCost;
-    public Node parent;
-    int heapIndex;
-    public int fCost
-    {
-        get
-        {
-            return gCost + hCost;
-        }
-    }
+	public bool walkable;
+	public Vector3 worldPosition;
+	public int gridX = 0;
+	public int gridY = 0;
+	public int gCost;
+	public int hCost;
+	public Vector2[] vertices;
+	public Node parent;
+	int heapIndex;
+	public int fCost
+	{
+		get
+		{
+			return gCost + hCost;
+		}
+	}
 
-    public int HeapIndex
-    {
-        get
-        {
-            return heapIndex;
-        }
-        set
-        {
-            heapIndex = value;
-        }
-    }
+	public int HeapIndex
+	{
+		get
+		{
+			return heapIndex;
+		}
+		set
+		{
+			heapIndex = value;
+		}
+	}
 
-    public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY)
-    {
-        this.walkable = walkable;
-        this.worldPosition = worldPosition;
-        this.gridX = gridX;
-        this.gridY = gridY;
-    }
+	public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY)
+	{
+		this.walkable = walkable;
+		this.worldPosition = worldPosition;
+		this.gridX = gridX;
+		this.gridY = gridY;
+	}
+	public Node(bool walkable, Vector2 worldPosition, int gridX, int gridY, Vector2[] vertices)
+	{
+		this.walkable = walkable;
+		this.worldPosition = worldPosition;
+		this.gridX = gridX;
+		this.gridY = gridY;
+		this.vertices = vertices;
+	}
 
-    public override bool Equals(object obj)
-    {
-        return base.Equals(obj);
-    }
-    public int CompareTo(Node other)
-    {
-        int compare = fCost.CompareTo(other.fCost);
-        if (compare == 0)
-        {
-            compare = hCost.CompareTo(other.hCost);
-        }
-        return -compare;
-    }
+	public override bool Equals(object obj)
+	{
+		return base.Equals(obj);
+	}
+	public int CompareTo(Node other)
+	{
+		int compare = fCost.CompareTo(other.fCost);
+		if (compare == 0)
+		{
+			compare = hCost.CompareTo(other.hCost);
+		}
+		return -compare;
+	}
 
-    public static bool operator ==(Node lhs, Node rhs)
-    {
-        return lhs.worldPosition == rhs.worldPosition && lhs.walkable == rhs.walkable;
-    }
-    public static bool operator !=(Node lhs, Node rhs)
-    {
-        return !(lhs == rhs);
-    }
+	public static bool operator ==(Node lhs, Node rhs)
+	{
+		return lhs.worldPosition == rhs.worldPosition && lhs.walkable == rhs.walkable;
+	}
+	public static bool operator !=(Node lhs, Node rhs)
+	{
+		return !(lhs == rhs);
+	}
+	public void DrawGizmos(float centerSize)
+	{
+		Gizmos.DrawWireSphere(worldPosition, centerSize);
+		Gizmos.DrawLine(vertices[0], vertices[1]);
+		Gizmos.DrawLine(vertices[1], vertices[2]);
+		Gizmos.DrawLine(vertices[2], vertices[3]);
+		Gizmos.DrawLine(vertices[3], vertices[0]);
+	}
 }

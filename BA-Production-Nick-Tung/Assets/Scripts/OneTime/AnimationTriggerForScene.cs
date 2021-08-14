5141f31c7a896d91272e7b781d51eb3592547d6a
Khassian <tungthanhcao1091995@gmail.com>
2021-08-14 12:12:49 +0200

    update settings for the animations

---
+++ BA-Production-Nick-Tung/Assets/Scripts/OneTime/AnimationTriggerForScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTriggerForScene : MonoBehaviour
+{
+	[SerializeField]
+	Animator animator;
+	[SerializeField]
+	string triggerName;
+	void Start()
+	{
+		animator.SetTrigger(triggerName);
+	}
+
+}

a56ccfdd3af0293e56003f7559b7fe97be6a439d
Khassian <tungthanhcao1091995@gmail.com>
2022-01-08 11:11:18 +0100

    implement new poses into the story

--- BA-Production-Nick-Tung/Assets/Scripts/ConversationMechanic/CharacterPosition.cs
+++ BA-Production-Nick-Tung/Assets/Scripts/ConversationMechanic/CharacterPosition.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 public class CharacterPosition : MonoBehaviour
@@ -6,4 +7,18 @@ public class CharacterPosition : MonoBehaviour
 	public string positionName;
 	public SpriteRenderer sprite;
 	public FadeTransition transition;
+	[Button]
+	public void Show()
+	{
+		var color = this.sprite.color;
+		color.a = 1;
+		this.sprite.color = color;
+	}
+	[Button]
+	public void Hide()
+	{
+		var color = this.sprite.color;
+		color.a = 0;
+		this.sprite.color = color;
+	}
 }
